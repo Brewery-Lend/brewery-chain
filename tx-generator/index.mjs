584eb10c38d7a3669b2d867e4646bdc446e5dcb7
Chanchal Delson <chanchaldelson@gmail.com>
2025-03-25 08:28:54 +0530

    Rollup Initiation

--- tx-generator/index.mjs
+++ tx-generator/index.mjs
@@ -1,10 +1,10 @@
 import { ethers } from "ethers";
 (async () => {
-    const provider = new ethers.JsonRpcProvider('http://YOUR_ROLLUP_RPC_URL:8547'); // Rollup RPC URL
-    const signer = new ethers.Wallet('FUNDED_PRIVATE_KEY_ON_YOUR_ROLLUP', provider);
+    const provider = new ethers.JsonRpcProvider('http://127.0.0.1:8547'); // Rollup RPC URL
+    const signer = new ethers.Wallet('4d592eba0460d9628207ed3e751bd8cfab8c08d057acd1ba37d69b8b4ec92d65', provider);
 
     const tx = await signer.sendTransaction({
-        to: 'DESTINATION_ADDRESS',
+        to: '0xb723A5D23E7BB52c28Db05a8e11498515C459b51',
         value: ethers.parseUnits('1', 'wei'),
         gasPrice: ethers.parseUnits('1', 'gwei'),
         gasLimit: 26000,
